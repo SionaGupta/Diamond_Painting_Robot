3c16f84053d939cc36ed131c5cfbbbcb77d1d473
SionaGupta <siona.gupta00@gmail.com>
2025-08-26 11:41:11 -0700

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,11 +1,22 @@
 # class for each drill on the canvas 
 class drill: 
-    def __init__(self, x, y):
+    def __init__(self, x, y, vector, color):
+        # xth drill from the left  
         self.x = x
+
+        #yth drill from the top 
         self.y = y
+
+        # symbol vector
+        self.vector = vector
+
+        # drill color 
+        self.color = color
 
 # class for each color 
 class color:
-    def __init__(self, colorNum, vector):
-        self.colorNum = colorNum
+    def __init__(self, number, vector):
+        # color number
+        self.number = number
+        # color symbol vector
         self.vector = vector
