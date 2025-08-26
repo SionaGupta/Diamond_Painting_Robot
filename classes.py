243c451c8b0e8dd38c62c552724f4506d2b79678
SionaGupta <siona.gupta00@gmail.com>
2025-08-26 11:18:46 -0700

    Creating classes

---
+++ classes.py
@@ -0,0 +1,11 @@
+# class for each drill on the canvas 
+class drill: 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# class for each color 
+class color:
+    def __init__(self, colorNum, vector):
+        self.colorNum = colorNum
+        self.vector = vector
