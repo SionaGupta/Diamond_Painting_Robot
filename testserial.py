2631f000488372e5d35dc11014d4518c65f4fa03
SionaGupta <siona.gupta00@gmail.com>
2025-10-27 21:56:29 -0700

    Added G-Code Parsing

--- testserial.py
+++ testserial.py
@@ -23,14 +23,30 @@ cords = [
 ser = serial.Serial('/dev/tty.usbserial-110', 9600, timeout=1)
 time.sleep(2)
 
-# Testing 
-ser.write(b"Hello World\n")
 
-# G-Code 
-for c in cords: 
-    ser.write(f"{c.x} {c.y}\n".encode())
-    print("moving to " + str(c.x) + " and " + str(c.y))
-    time.sleep(4.   )
+for c in cords:
+    # Move to drill supply location
+    drill_x, drill_y = 10, 20  # example supply location, To be Changed to reflect changing drill pick up locations
+
+    ser.write(f"G0 X{drill_x} Y{drill_y}\n".encode())
+    print(f"Moving to drill supply")
+    time.sleep(4)  
+
+    # Pick up drill
+    ser.write("M3\n".encode())
+    print("Picking up drill")
+    time.sleep(4)  # wait for servo
+
+    # Move to canvas position
+    ser.write(f"G01 X{c.x} Y{c.y}\n".encode())
+    print(f"Moving to canvas: X{c.x} Y{c.y}")
+    time.sleep(4)
+
+    # Place drill
+    ser.write("M3\n".encode())
+    print("Placing drill")
+    time.sleep(4)
+
 
 ser.close()
 
